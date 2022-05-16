3a824d7630d228aa59169668aa0b3ce2c9ec7ae7
wabibaah <wabisernproject@gmail.com>
2022-05-16 17:09:35 +0000

    photos model and carousel to the post details page.

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,4 @@
+
 from django.db import models
 from django.contrib.auth.models import User
 from profiles.models import Profile
@@ -19,5 +20,19 @@ class Post(models.Model):
   def like_count(self):
     return self.liked.all().count()
 
+  def get_photos(self):
+    return self.photo_set.all()
+  ### this is reverse relationship because we have an instance of the post and we want to find all the photos that are related to it. self refers to any instance of the post and photo is the small letters of Photo. 
+
   class Meta:
     ordering = ('-created',)
+
+
+class Photo(models.Model):
+  post = models.ForeignKey(Post, on_delete=models.CASCADE)
+  image = models.ImageField(upload_to="photos")
+  created = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+      return f"{ self.post.title } - { self.pk }"
+  
